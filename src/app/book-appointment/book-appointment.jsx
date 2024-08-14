b9e4c5b9ef0e954d608bb8108be61ceacde5ef80
Apoorva <apoorvaarza94@gmail.com>
2024-08-14 15:24:09 +0530

    removed pii

--- src/app/book-appointment/book-appointment.jsx
+++ src/app/book-appointment/book-appointment.jsx
@@ -7,19 +7,21 @@ const BookAppointment = () => {
     <section className={styles.container} id="book-appointment">
       <h1>Book an Appointment Today</h1>
 
-      <a className='block py-5' href="tel: +91 8885658577">
+      <a className='block py-5' >
+      {/* <a className='block py-5' href="tel:"> */}
         <FontAwesomeIcon
           style={{ fontSize: "24px", paddingRight: '16px' }}
           icon={faPhone}
         ></FontAwesomeIcon>
-        8885658577
+        TBC
       </a>
-      <a className='block py-5' href="mailto:apoorvaarza94@gmail.com">
+      {/* <a className='block py-5' href="mailto:"> */}
+      <a className='block py-5' >
         <FontAwesomeIcon
           style={{ fontSize: "25px", paddingRight: '16px' }}
           icon={faEnvelope}
         ></FontAwesomeIcon>
-        apoorvaarza94
+        TBC
       </a>
       <p className="py-5">Address: TBC H.no</p>
     </section>
